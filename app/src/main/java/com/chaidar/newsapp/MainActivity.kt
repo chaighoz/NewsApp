d5e4c155350701cf4e1adde93872a35fea965e3e
Chaidar ghozwu el-haqqi <ghozwuchaidar@gmail.com>
2021-11-01 08:17:48 +0700

    Saya menambahkan TabLayoutMediator

--- app/src/main/java/com/chaidar/newsapp/MainActivity.kt
+++ app/src/main/java/com/chaidar/newsapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.chaidar.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private var mSectionPagerAdapter : SectionPagerAdapter? = null
+    private lateinit var vpnews : ViewPager2
+    private lateinit var tabs : TabLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.tool_bar))
+
+        vpnews  = findViewById(R.id.vp_news)
+        tabs = findViewById(R.id.tabs)
+
+        mSectionPagerAdapter = SectionPagerAdapter(this)
+        vpnews.adapter = mSectionPagerAdapter
+
+        val tabList = arrayOf("All News", "Top", "Popular")
+        TabLayoutMediator(tabs, vpnews){tab, position ->
+            tab.text = tabList[position]
+        }.attach()
     }
 }
---
+++ app/src/main/java/com/chaidar/newsapp/SplashScreenActivity.kt
@@ -0,0 +1,14 @@
+package com.chaidar.newsapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
